2b6507662d0ede13e34d32ece26c0dbb6e3559f4
Yash Tiwari <yashtiwari.coderkube@gmail.com>
2024-11-30 12:23:39 +0530

    cors

--- dist/app.js
+++ dist/app.js
@@ -6,9 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const tutorial_route_1 = __importDefault(require("./routes/tutorial.route"));
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 app.use("/api", tutorial_route_1.default);
 const PORT = process.env.PORT || 4001;
 app.listen(PORT, () => {
