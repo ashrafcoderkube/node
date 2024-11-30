30e14ad3e4c90a1cc6be4a15e5afdc41f2f12c79
Yash Tiwari <yashtiwari.coderkube@gmail.com>
2024-11-30 13:27:46 +0530

    health check : server

--- dist/app.js
+++ dist/app.js
@@ -12,6 +12,10 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 app.use("/api", tutorial_route_1.default);
+app.get("/health", (req, res) => {
+    res.send("server started");
+    return;
+});
 const PORT = process.env.PORT || 4001;
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
-import router from "./routes/tutorial.route"
-import cors from "cors"
+import router from "./routes/tutorial.route";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.use("/api", router);
+
+app.get("/health", (req: express.Request, res: express.Response) => {
+  res.send("server started");
+  return;
+});
 
 const PORT = process.env.PORT || 4001;
 app.listen(PORT, () => {
