15cbee5f0f913e1d5f51693531139404941d2498
Yash Tiwari <yashtiwari.coderkube@gmail.com>
2024-11-30 14:55:19 +0530

    project init 🚀

--- Routes/Category.Route.js
+++ Routes/Category.Route.js
@@ -1,17 +1,13 @@
 
 const express = require("express");
-// Creating a router instance from express to define route handlers
 const router = express.Router();
 
-// Importing the Controllers 
 const CategoryController = require("../Controllers/Category.Controller")
 
-// Defining a route 
 router.post("/" ,CategoryController.createCategory);
 router.get("/" ,CategoryController.getCategory);
 router.put("/" ,CategoryController.updateCategory);
 router.delete("/" ,CategoryController.deleteCategory);
 
-// Exporting the router instance to be used in other parts of the application
 module.exports = router;
               
--- Routes/Library.Route.js
+++ Routes/Library.Route.js
@@ -1,17 +1,13 @@
 
 const express = require("express");
-// Creating a router instance from express to define route handlers
 const router = express.Router();
 
-// Importing the Controllers 
 const LibraryController = require("../Controllers/Library.Controller")
 
-// Defining a route 
 router.post("/" ,LibraryController.createLibrary);
 router.get("/" ,LibraryController.getLibrary);
 router.put("/" ,LibraryController.updateLibrary);
 router.delete("/" ,LibraryController.deleteLibrary);
 
-// Exporting the router instance to be used in other parts of the application
 module.exports = router;
               
