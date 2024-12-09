95b4919b3d41091fd363668720db911580640f43
ctrly4sh <yashtiwari.coderkube@gmail.com>
2024-12-09 16:37:41 +0530

    featured-tutorials api

--- Controllers/Feature.Controller.js
+++ Controllers/Feature.Controller.js
@@ -3,15 +3,35 @@ const ResponseHandler = require("../Utils/responseHandler");
 const TutorialsModel = require("../Models/Tutorials.Model");
 
 module.exports = {
-  getFeaturedTutorials: async (req, res) => {
+  getFeaturedTutorials: async (req, res, next) => {
     try {
-      // const tutorials = await TutorialsModel.aggregate([
-      //   { $sample: { size: 6 } },
-      // ]);
+      const result = await TutorialsModel.aggregate([
+        { $sample: { size: 6 } },
+        {
+          $lookup: {
+            from: "categories", // Name of the collection in the database for `category`
+            localField: "category", // Field in `TutorialsModel` referencing the category
+            foreignField: "_id", // Field in `categories` that matches `category`
+            as: "category", // Name of the resulting array field
+          },
+        },
+        {
+          $lookup: {
+            from: "difficulties", // Name of the collection in the database for `difficulty`
+            localField: "difficulty", // Field in `TutorialsModel` referencing the difficulty
+            foreignField: "_id", // Field in `difficulties` that matches `difficulty`
+            as: "difficulty", // Name of the resulting array field
+          },
+        },
+        {
+          $project: {
+            "category._id": 0, // Optionally hide fields, like `_id`, in the category
+            "difficulty._id": 0, // Optionally hide fields in the difficulty
+          },
+        },
+      ]);
 
-      const result = await TutorialsModel.aggregate([{ $sample: { size: 6 } }]);
-
-      if (!result) {
+      if (!result || result.length === 0) {
         ResponseHandler.sendError(
           res,
           "Data not found",
@@ -20,22 +40,21 @@ module.exports = {
         );
         return;
       }
+
       ResponseHandler.sendSuccess(
         res,
         result,
         Codes.OK,
         Messages.DATA_RETRIEVED_SUCCESS
       );
-      return;
     } catch (error) {
       ResponseHandler.sendError(
         res,
-        error,
+        error.message,
         Codes.INTERNAL_SERVER_ERROR,
         Messages.INTERNAL_SERVER_ERROR
       );
       next(error);
-      return;
     }
   },
 };
